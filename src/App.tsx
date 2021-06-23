924c638b94edbe4bfb42ad5036c99f0a7e23005e
Shayne Thiessen <shayne@shaynethiessen.com>
2021-06-23 11:32:50 -0500

    Added map
    close #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import {MapContainer, Marker, Popup, TileLayer} from '@monsonjeremy/react-leaflet'
+import {Marker as LMarker, icon as LIcon} from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/leaflet.css';
+
+let DefaultIcon = LIcon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+LMarker.prototype.options.icon = DefaultIcon;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Terrene
-        </p>
-      </header>
-    </div>
+    return (
+      <div style={{height: '100vh'}}>
+          <MapContainer center={{lat: 51, lng: 10}} zoom={13} style={{height: '100vh'}}>
+              <TileLayer
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={{lat: 51, lng: 10}}>
+                  <Popup>
+                      A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+              </Marker>
+          </MapContainer>
+      </div>
   );
 }
 
