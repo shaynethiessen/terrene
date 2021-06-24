fecdb461a311b3cc703aa8ac1abe49239396f4a3
Shayne Thiessen <shayne@shaynethiessen.com>
2021-06-24 12:42:00 -0500

    Set default zoom and added default data
    close #8

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import {MapContainer, Marker, Popup, TileLayer} from '@monsonjeremy/react-leaflet'
 import {MainMenu} from "./MainMenu";
+import {historicSites} from './SampleDB';
 
 function App() {
     return (
         <div style={{height: 'calc(100% - 40px)'}}>
             <MainMenu />
-            <MapContainer center={{lat: 51, lng: 10}} zoom={13} style={{height: '100%'}}>
+            <MapContainer center={{lat: 46, lng: -70}} zoom={3} style={{height: '100%'}} minZoom={3}>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={{lat: 51, lng: 10}}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {historicSites.map(site => {
+                    return (
+                        <Marker position={{lat: site.lat, lng: site.lng}}>
+                            <Popup>
+                                {site.name}
+                            </Popup>
+                        </Marker>
+                    );
+                })}
             </MapContainer>
         </div>
   );
---
+++ src/SampleDB.ts
@@ -0,0 +1,12 @@
+export const historicSites =[
+    {
+        lat: 46.809444,
+        lng: -71.210556,
+        name: 'Historic District of Old Québec'
+    },
+    {
+        lat: 49.075,
+        lng: -111.633056,
+        name: 'Writing-on-Stone / Áísínai\'pi'
+    }
+]
