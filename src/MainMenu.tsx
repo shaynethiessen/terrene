a8078b2d936fb5d607baf9f6fe4491f1addc4dd3
Shayne Thiessen <shayne@shaynethiessen.com>
2021-06-24 09:43:20 -0500

    Added simple menu

---
+++ src/MainMenu.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Menu} from "semantic-ui-react";
+
+export function MainMenu() {
+    return (
+        <Menu inverted attached>
+            <Menu.Item name="Home" icon="home" />
+        </Menu>
+    )
+}
