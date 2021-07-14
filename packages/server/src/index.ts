d22bd2e6b482d1a02065c51386d14503119ca363
Shayne Thiessen <shayne@shaynethiessen.com>
2021-07-14 14:45:27 -0500

    removed console statements

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -11,13 +11,11 @@ const PORT = 3001;
 app.use(cors());
 
 app.listen(PORT, () => {
-	console.log('running');
 	d(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
 
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 
 app.post('/historic-sites', (req, res) => {
-	console.log('test');
 	res.send(historicSites).status(200);
 });
