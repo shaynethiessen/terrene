45f05ac067f9eaa7967771261108710e18f86256
Shayne Thiessen <shayne@shaynethiessen.com>
2021-12-20 10:06:47 -0600

    Updated casing in attributions

--- packages/server/src/SampleDB.ts
+++ packages/server/src/SampleDB.ts
@@ -9,7 +9,7 @@ export const historicSites: HistoricSiteType[] = [
 		slug: 'historic-district-of-old-quebec',
 		content:
 			'Québec was founded by the French explorer Champlain in the early 17th century. It is the only North American city to have preserved its ramparts, together with the numerous bastions, gates and defensive works which still surround Old Québec. The Upper Town, built on the cliff, has remained the religious and administrative centre, with its churches, convents and other monuments like the Dauphine Redoubt, the Citadel and Château Frontenac. Together with the Lower Town and its ancient districts, it forms an urban ensemble which is one of the best examples of a fortified colonial city.',
-		attribution: 'source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
+		attribution: 'Source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
 	},
 	{
 		id: 2,
@@ -19,7 +19,7 @@ export const historicSites: HistoricSiteType[] = [
 		slug: 'ridaeu-canal',
 		content:
 			'The Rideau Canal, a monumental early 19th-century construction covering 202 km of the Rideau and Cataraqui rivers from Ottawa south to Kingston Harbour on Lake Ontario, was built primarily for strategic military purposes at a time when Great Britain and the United States vied for control of the region. The site, one of the first canals to be designed specifically for steam-powered vessels, also features an ensemble of fortifications. It is the best-preserved example of a slackwater canal in North America, demonstrating the use of this European technology on a large scale. It is the only canal dating from the great North American canal-building era of the early 19th century to remain operational along its original line with most of its structures intact.',
-		attribution: 'source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
+		attribution: 'Source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
 	},
 	{
 		id: 3,
@@ -29,7 +29,7 @@ export const historicSites: HistoricSiteType[] = [
 		slug: 'sgang-gwaay-llanagaay',
 		content:
 			"The village of Ninstints (Nans Dins) is located on a small island off the west coast of the Queen Charlotte Islands (Haida Gwaii). Remains of houses, together with carved mortuary and memorial poles, illustrate the Haida people's art and way of life. The site commemorates the living culture of the Haida people and their relationship to the land and sea, and offers a visual key to their oral traditions.",
-		attribution: 'source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
+		attribution: 'Source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
 	},
 	{
 		id: 4,
@@ -39,7 +39,7 @@ export const historicSites: HistoricSiteType[] = [
 		slug: 'writing-on-stone-aisinaipi',
 		content:
 			'The property is located on the northern edge of the semi-arid Great Plains of North America, on the border between Canada and the United States of America. The Milk River Valley dominates the topography of this cultural landscape, which is characterized by a concentration of pillars or hoodoos – columns of rock sculpted by erosion into spectacular shapes. The Blackfoot Confederacy (Siksikáíítsitapi) left engravings and paintings on the sandstone walls of the Milk River Valley, bearing testimony to messages from Sacred Beings. Dated in situ archaeological remains cover a period between ca. 4,500 BP - 3,500 years BP and the Contact Period. This landscape is considered sacred to the Blackfoot people, and their centuries-old traditions are perpetuated through ceremonies and in enduring respect for the places',
-		attribution: 'source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
+		attribution: 'Source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
 	},
 	{
 		id: 5,
@@ -49,7 +49,7 @@ export const historicSites: HistoricSiteType[] = [
 		slug: 'sydney-opera-house',
 		content:
 			'Inaugurated in 1973, the Sydney Opera House is a great architectural work of the 20th century that brings together multiple strands of creativity and innovation in both architectural form and structural design. A great urban sculpture set in a remarkable waterscape, at the tip of a peninsula projecting into Sydney Harbour, the building has had an enduring influence on architecture. The Sydney Opera House comprises three groups of interlocking vaulted ‘shells’ which roof two main performance halls and a restaurant. These shell-structures are set upon a vast platform and are surrounded by terrace areas that function as pedestrian concourses. In 1957, when the project of the Sydney Opera House was awarded by an international jury to Danish architect Jørn Utzon, it marked a radically new approach to construction.',
-		attribution: 'source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
+		attribution: 'Source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
 	},
 	{
 		id: 6,
@@ -59,6 +59,6 @@ export const historicSites: HistoricSiteType[] = [
 		slug: 'la-grand-place-brussels',
 		content:
 			'La Grand-Place in Brussels is a remarkably homogeneous body of public and private buildings, dating mainly from the late 17th century. The architecture provides a vivid illustration of the level of social and cultural life of the period in this important political and commercial centre.',
-		attribution: 'source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
+		attribution: 'Source: uneseco.org, licensed under CC-BY-SA IGO 3.0',
 	},
 ];
