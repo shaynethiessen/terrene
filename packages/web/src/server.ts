eea263b6e56a570039cb8436b6935fb6137f9feb
Shayne Thiessen <shayne@shaynethiessen.com>
2021-12-21 08:18:45 -0600

    localhost

--- packages/web/src/server.ts
+++ packages/web/src/server.ts
@@ -4,7 +4,7 @@ const d = debug('web.src.server');
 
 export const server = {
 	fetch: async (location: string, body?: Record<string, unknown>) => {
-		const response = await fetch(`https://0.0.0.0:3001/${location}`, {
+		const response = await fetch(`http://localhost:3001/${location}`, {
 			method: 'POST',
 			headers: {
 				'Content-type': 'application/json',
