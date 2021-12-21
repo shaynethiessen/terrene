93c2c56983e7b8db5a748deed728ef2e63c0d982
Shayne Thiessen <shayne@shaynethiessen.com>
2021-12-21 09:52:08 -0600

    Update server.ts

--- packages/web/src/server.ts
+++ packages/web/src/server.ts
@@ -4,7 +4,7 @@ const d = debug('web.src.server');
 
 export const server = {
 	fetch: async (location: string, body?: Record<string, unknown>) => {
-		const response = await fetch(`http://localhost:3001/${location}`, {
+		const response = await fetch(`https://terrene-server.herokuapp.com/`, {
 			method: 'POST',
 			headers: {
 				'Content-type': 'application/json',
