4af30b0f0a731e03a7b2fcd005b9092f092c7c60
Shayne Thiessen <shayne@shaynethiessen.com>
2021-12-25 21:48:07 -0600

    Updated menu

--- packages/web/src/Layout/MainMenu.tsx
+++ packages/web/src/Layout/MainMenu.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import {Icon, Menu} from 'semantic-ui-react';
 import logo from './logo.png';
 
 export function MainMenu() {
+	const {pathname} = useLocation();
+
 	return (
-		<Menu inverted attached stackable style={{align: 'center'}}>
+		<Menu inverted attached>
 			<Menu.Item name="logo">
 				<img src={logo} alt="Logo" style={{paddingRight: '0.6em'}} />
 			</Menu.Item>
-			<Menu.Item name="home">
+			<Menu.Item name="home" active={pathname === '/'}>
 				<Link to="/">Home</Link>
 			</Menu.Item>
-			<Menu.Item name="login" position="right">
-				<Link to="/login">
-					<Icon name="user circle" color="yellow" size="large" />
-				</Link>
-			</Menu.Item>
+			<Menu.Menu position="right">
+				<Menu.Item name="login" active={pathname === '/login'}>
+					<Link to="/login">
+						<Icon name="user circle" color="yellow" size="large" />
+					</Link>
+				</Menu.Item>
+			</Menu.Menu>
 		</Menu>
 	);
 }
