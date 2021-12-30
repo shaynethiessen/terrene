be7ee0ffe1e25a6b6925151c98d480655d40dcc1
Shayne Thiessen <shayne@shaynethiessen.com>
2021-12-30 11:25:14 -0600

    Added user location to the map

--- packages/web/src/Pages/Home/HistoricSiteMarkers.tsx
+++ packages/web/src/Pages/Home/HistoricSiteMarkers.tsx
@@ -3,20 +3,26 @@ import {Marker, Popup, useMap} from '@monsonjeremy/react-leaflet';
 import {Link} from 'react-router-dom';
 import {Header, Image} from 'semantic-ui-react';
 import type {HistoricSiteType} from 'terrene-types';
-import {getImage} from '../../lib/getImage';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import {icon as LIcon} from 'leaflet';
+import userIcon from './userIcon.png';
+import historicIcon from './historicIcon.png';
 import {server} from '../../core/server';
+import {getImage} from '../../lib/getImage';
 
 export function HistoricSiteMarkers() {
 	const [historicSites, setHistoricSites] = useState<HistoricSiteType[]>();
 	const [lat, setLat] = useState(46);
 	const [lng, setLng] = useState(-70);
 	const [zoom, setZoom] = useState(3);
+	const [userLocationEnabled, setUserLocationEnabled] = useState(false);
 
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
 			setLat(position.coords.latitude);
 			setLng(position.coords.longitude);
 			setZoom(5);
+			setUserLocationEnabled(true);
 		});
 	}, [navigator.geolocation]);
 
@@ -30,11 +36,35 @@ export function HistoricSiteMarkers() {
 
 	map.setView({lat, lng}, zoom);
 
+	const UserIcon = LIcon({
+		iconUrl: userIcon,
+		shadowUrl: iconShadow,
+		iconSize: [25, 41],
+		iconAnchor: [10, 41],
+		popupAnchor: [2, -40],
+	});
+
+	const HistoricSiteIcon = LIcon({
+		iconUrl: historicIcon,
+		shadowUrl: iconShadow,
+		iconSize: [25, 41],
+		iconAnchor: [10, 41],
+		popupAnchor: [2, -40],
+	});
+
 	return (
 		<>
+			{userLocationEnabled && (
+				<Marker position={{lat, lng}} icon={UserIcon}>
+					<Popup>
+						<Header>My Location</Header>
+						<Image src={userIcon} />
+					</Popup>
+				</Marker>
+			)}
 			{historicSites?.map(site => {
 				return (
-					<Marker position={{lat: site.latitude, lng: site.longitude}} key={site.id}>
+					<Marker position={{lat: site.latitude, lng: site.longitude}} key={site.id} icon={HistoricSiteIcon}>
 						<Popup>
 							<Link to={`historic-site/${site.slug}`}>
 								<Header>{site.name}</Header>
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-import {Marker as LMarker, icon as LIcon} from 'leaflet';
 import debug from 'debug';
 import App from './App';
 
@@ -11,15 +8,5 @@ import 'leaflet/dist/leaflet.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 const d = debug('app.web');
-
-const DefaultIcon = LIcon({
-	iconUrl: icon,
-	shadowUrl: iconShadow,
-	iconSize: [25, 41],
-	iconAnchor: [10, 41],
-	popupAnchor: [2, -40],
-});
-
-LMarker.prototype.options.icon = DefaultIcon;
 
 ReactDOM.render(<App />, document.getElementById('root'));
