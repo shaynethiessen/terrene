03fccc300fee3ea7dbbcadb207575f9ba6ac6ed6
Shayne Thiessen <shayne@shaynethiessen.com>
2022-01-02 15:50:40 -0600

    Prevented scrolling map infinitly north/south
    Closed #40

--- packages/web/src/Pages/Home/Content.tsx
+++ packages/web/src/Pages/Home/Content.tsx
@@ -7,7 +7,18 @@ const d = debug('web.src.app.home');
 
 export function Content() {
 	return (
-		<MapContainer center={{lat: 46, lng: -70}} zoom={3} style={{height: '100%'}} minZoom={3}>
+		<MapContainer
+			center={{lat: 46, lng: -70}}
+			zoom={3}
+			style={{height: '100%'}}
+			minZoom={3}
+			maxBounds={[
+				[-90, -Infinity],
+				[90, Infinity],
+			]}
+			maxBoundsViscosity={1}
+			worldCopyJump
+		>
 			<TileLayer
 				attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community"
 				url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
