25fa2cac74bd154de8aaedb41dab97ea2a38b791
Shayne Thiessen <shayne@shaynethiessen.com>
2022-01-08 19:23:16 -0600

    Update organize.sh

--- tools/organize.sh
+++ tools/organize.sh
@@ -1,79 +1,13 @@
 #!/usr/bin/env bash
 
-TOOLS_DIR='/tools/'
-source "$TOOLS_DIR/bin/common.sh"
+TOOLS_DIR='/tools'
 
-show_help () {
-  printf "${LCYAN}Organize Script${NC}\n\n"
-  printf "Usage:  organize.sh [OPTIONS]\n\n"
-	printf "A helper script for organizing your code.\n"
-	printf "Runs the following tools:\n"
-	printf "  - Sorts all package.json files\n"
-	printf "  - Codemod to sort import statements\n"
-	printf "  - Codemod to rename alias imports\n"
-	printf "  - Codemod to fix debug namespaces\n"
-	printf "  - Fixes lint issues\n\n"
-	printf "Options:\n"
-	printf "  -p  string  Packages directory [required]\n"
-	printf "  -d  string  Debug namespace root [required]\n"
-	printf "  -i  string  Comma separated list of package dirs to ignore\n"
-	printf "\n"
-}
-
-# A POSIX variable
-OPTIND=1 # Reset in case getopts has been used previously in the shell.
-
-# Command line variable defaults
-PACKAGE_DIR=""
-IGNORE_DIRS=""
-DEBUG_NAMESPACE=""
-
-while getopts "h?p:i:d:" opt; do
-	case "$opt" in
-	h|\?)
-		show_help
-		exit 0
-		;;
-	p)  PACKAGE_DIR=$OPTARG
-		;;
-  i)  IGNORE_DIRS=$OPTARG
-    ;;
-  d)  DEBUG_NAMESPACE=$OPTARG
-    ;;
-	esac
-done
-shift $((OPTIND-1))
-[ "${1:-}" = "--" ] && shift
-
-C=( "dos2unix" "git" )
-check_cmds C
-
-# Make sure a packages dir is specified
-if [ -z "$PACKAGE_DIR" ]; then
-  error "Packages directory not specified with -p"
-  exit 1
-fi
-
-# Get comma separated package ignores into array
-split_csv "$IGNORE_DIRS" IGNORE_PKGS
-SRCDIRS=$(get_package_folders_string "$PACKAGE_DIR" IGNORE_PKGS '/src')
-
-#banner "Fixing jscodeshift"
-JSCODESHIFT_DIR=$(get_jscodeshift_dir "$TOOLS_DIR")
-fix_jscodeshift "$JSCODESHIFT_DIR" "$TOOLS_DIR"
-restore_cwd
-
-banner "Sorting package.json files"
-yarn -s sort
-
-banner "Codemod"
-echo "${SRCDIRS}" | DEBUG_NAMESPACE="$DEBUG_NAMESPACE" "$JSCODESHIFT_DIR/bin/jscodeshift.js" --extensions=tsx,ts --parser=tsx -t "$TOOLS_DIR/files/cmOrganize.ts" --stdin
+echo "${SRCDIRS}" | DEBUG_NAMESPACE="$DEBUG_NAMESPACE" "node_modules/jscodeshift/bin/jscodeshift.js" --extensions=tsx,ts --parser=tsx -t "$TOOLS_DIR/cmOrganize.ts" --stdin
 ret=$?
 if [ $ret -ne 0 ]; then
   error "Error running codemod. Lint fixing will continue to prevent many changed files."
 fi
 
-banner "Fixing lint issues"
 yarn -s lint:fix
 
 if [ $ret -ne 0 ]; then
@@ -81,5 +15,4 @@ if [ $ret -ne 0 ]; then
   exit $ret
 fi
 
-banner "Organize complete"
 exit 0
