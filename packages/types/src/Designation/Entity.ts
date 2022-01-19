8884f39b0f001bca13f8a5a661e04c27b00d2b2d
Shayne Thiessen <shayne@shaynethiessen.com>
2022-01-19 16:24:43 -0600

    Improved list of provincial historic site designations
    Closed #114

--- packages/types/src/Designation/Entity.ts
+++ packages/types/src/Designation/Entity.ts
@@ -1,7 +1,19 @@
 export enum DesignationTypeEnum {
+	UnescoWorldHeritageSite = 'UNESCO World Heritage Site',
 	NationalHistoricSiteOfCanada = 'National Historic Site of Canada',
 	ProvincialHistoricSiteOfAlberta = 'Provincial Historic Site of Alberta',
-	UnescoWorldHeritageSite = 'UNESCO World Heritage Site',
+	ProvincialHistoricSiteOfBritishColumbia = 'Provincial Historic Site of British Columbia',
+	ProvincialHistoricSiteOfManitoba = 'Provincial Historic Site of Manitoba',
+	ProvincialHistoricSiteOfNewBrunswick = 'Provincial Historic Site of New Brunswick',
+	ProvincialHistoricSiteOfNewfoundlandandLabrador = 'Provincial Historic Site of Newfoundland and Labrador',
+	ProvincialHistoricSiteOfNorthwestTerritories = 'Provincial Historic Site of Northwest Territories',
+	ProvincialHistoricSiteOfNovaScotia = 'Provincial Historic Site of Nova Scotia',
+	ProvincialHistoricSiteOfNunavut = 'Provincial Historic Site of Nunavut',
+	ProvincialHistoricSiteOfOntario = 'Provincial Historic Site of Ontario',
+	ProvincialHistoricSiteOfPrinceEdwardIsland = 'Provincial Historic Site of Prince Edward Island',
+	ProvincialHistoricSiteOfQuebec = 'Provincial Historic Site of Quebec',
+	ProvincialHistoricSiteOfSaskatchewan = 'Provincial Historic Site of Saskatchewan',
+	ProvincialHistoricSiteOfYukon = 'Provincial Historic Site of Yukon',
 }
 
 export type DesignationEntityConstructor = {
